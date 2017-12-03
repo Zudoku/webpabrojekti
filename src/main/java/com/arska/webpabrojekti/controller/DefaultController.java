b058394fcd673f6a2b8435cdd690d52fd2a1abbe
Arap <arttu.siren@gmail.com>
2017-12-03 18:19:20 +0200

    initial commit

---
+++ src/main/java/com/arska/webpabrojekti/controller/DefaultController.java
@@ -0,0 +1,22 @@
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Created Dec 3, 2017
+ * @author arska
+ */
+@Controller
+public class DefaultController {
+
+    @RequestMapping("*")
+    public String handleDefault() {
+        return "menu";
+    }
+}
---
+++ src/test/java/com/arska/webpabrojekti/NoTests.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.arska.webpabrojekti;
+
+import org.junit.Test;
+
+/**
+ * Created Dec 3, 2017
+ * @author arska
+ */
+
+public class NoTests {
+    
+    @Test
+    public void noTests(){
+        
+    }
+}
