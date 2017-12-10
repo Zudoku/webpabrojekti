551e1f765e36a1a48940a2c8e8bd951982d146ac
Arap <arttu.siren@gmail.com>
2017-12-11 00:14:57 +0200

    Lisää travis linkki readmehen ja lisää testidataa

--- README.md
+++ README.md
@@ -7,4 +7,6 @@ Github -> Travis ci -> Heroku
 
 ![alt text](https://travis-ci.org/Zudoku/webpabrojekti.svg?branch=master "branch: master")
 
+Travis: [https://travis-ci.org/Zudoku/webpabrojekti](https://travis-ci.org/Zudoku/webpabrojekti)
+
 Current stable build found here: [https://uutisbrojekti.herokuapp.com/](https://uutisbrojekti.herokuapp.com/)
--- src/main/java/com/arska/webpabrojekti/InitialDataLoader.java
+++ src/main/java/com/arska/webpabrojekti/InitialDataLoader.java
@@ -17,10 +17,7 @@ import java.io.IOException;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.nio.file.Files;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Random;
+import java.util.*;
 
 import com.google.common.io.ByteStreams;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -50,6 +47,10 @@ public class InitialDataLoader implements ApplicationRunner {
     
     
     private UutisKategoria urheilu;
+    private UutisKategoria viihde;
+    private UutisKategoria terveys;
+    private UutisKategoria politiikka;
+    private UutisKategoria some;
     
     private Random r = new Random();
 
@@ -62,22 +63,111 @@ public class InitialDataLoader implements ApplicationRunner {
     
     private void setUpKategoria(){
         urheilu = new UutisKategoria(true, "Urheilu", new ArrayList<>());
+        terveys = new UutisKategoria(true, "Terveys", new ArrayList<>());
+        some = new UutisKategoria(true, "Sosiaalinen media", new ArrayList<>());
+        viihde = new UutisKategoria(true, "Viihde", new ArrayList<>());
+        politiikka = new UutisKategoria(false, "Politiikka", new ArrayList<>());
         kategoriaRepository.save(urheilu);
-        kategoriaRepository.save(new UutisKategoria(true, "Viihde", new ArrayList<>()));
-        kategoriaRepository.save(new UutisKategoria(true, "Terveys", new ArrayList<>()));
-        kategoriaRepository.save(new UutisKategoria(true, "Sosiaalinen media", new ArrayList<>()));
-        kategoriaRepository.save(new UutisKategoria(false, "Politiikka", new ArrayList<>()));
+        kategoriaRepository.save(terveys);
+        kategoriaRepository.save(viihde);
+        kategoriaRepository.save(some);
+        kategoriaRepository.save(politiikka);
     }
 
     private void setUpUutinen() throws IOException, URISyntaxException {
         byte[] newsPicData = ByteStreams.toByteArray(resourceLoader.getClassLoader().getResourceAsStream("news.jpg"));
         
-        
-        
-        Uutinen u = new Uutinen("Suomalainen jalkapalloilija sai kultaa", "Suomalainen jalkapalloilija Tero teronen on voittanut EM-kultaa.", 
-                "Suomalainen jalkapalloilija Tero teronen on voittanut EM-kultaa. Hän ei vastannut haastattelupyyntöihimme.", 
-                System.currentTimeMillis(), System.currentTimeMillis(), newsPicData, r.nextInt(200), Arrays.asList(urheilu), getRandomToimittaja());
-        
+        setUpUrheilu(newsPicData);
+        setUpTerveys(newsPicData);
+        ///setUpSosiaalinenMedia(newsPicData);
+        //setUpViihde(newsPicData);
+    }
+
+    private void setUpTerveys(byte[] newsPicData) {
+        Uutinen u = new Uutinen("2 Niksiä flunssaan", "Asiantuntijan vinkit.",
+                "1. Lepo. 2. Uni.",
+                System.currentTimeMillis() - (r.nextInt(5) * 1000 * 60 * 60 * 24), System.currentTimeMillis(), newsPicData, r.nextInt(200),
+                Collections.singletonList(terveys), getRandomToimittaja());
+
+        uutisRepository.save(u);
+
+        u = new Uutinen("Syöpään on löydetty uusi hoito", "Ruotsissa asiantuntijat ovat löytäneet uuden hoidon ihosyöpään.",
+                "Ruotsissa Tukholman yliopistollisen sairaalan tutkijaryhmä on saanut positiivisia tuloksia hoitaessaan ihosyöpää uusilla tavoin.",
+                System.currentTimeMillis() - (r.nextInt(5) * 1000 * 60 * 60 * 24), System.currentTimeMillis(), newsPicData, r.nextInt(200),
+                Collections.singletonList(terveys), getRandomToimittaja());
+
+        uutisRepository.save(u);
+
+        u = new Uutinen("Vain kolmas-osa suomalaisista osaisi elvyttää", "Hätätilanteessa elvytys on arvokas taito.",
+                "Suomalaisista vain kolmas osa osaisi elvyttää arvioi Suomen terveysliitto ST.",
+                System.currentTimeMillis() - (r.nextInt(5) * 1000 * 60 * 60 * 24), System.currentTimeMillis(), newsPicData, r.nextInt(200),
+                Collections.singletonList(terveys), getRandomToimittaja());
+
+        uutisRepository.save(u);
+
+        u = new Uutinen("Stressin oireet ovat muuttumassa", "Uusi tutkimus osoittaa.",
+                "Stressin oireet ovat muuttuneet viime vuosikymmenen aikana toteaa Suomen terveysliitto ST.",
+                System.currentTimeMillis() - (r.nextInt(5) * 1000 * 60 * 60 * 24), System.currentTimeMillis(), newsPicData, r.nextInt(200),
+                Collections.singletonList(terveys), getRandomToimittaja());
+
+        uutisRepository.save(u);
+
+        u = new Uutinen("Opiskelijoiden univaje johtaa stressiin", "Monet nuoret kokevat kärsivänsä univajeesta.",
+                "Monet nuoret kokevat kärsivänsä univajeesta. Tohtori T.T Suomen terveysliitosta suosittelee nukkumaan tarpeeksi.",
+                System.currentTimeMillis() - (r.nextInt(5) * 1000 * 60 * 60 * 24), System.currentTimeMillis(), newsPicData, r.nextInt(200),
+                Collections.singletonList(terveys), getRandomToimittaja());
+
+        uutisRepository.save(u);
+
+        u = new Uutinen("30 minuuttia terveellistä liikuntaa päivässä tuo noin 5 vuotta lisää elinaikaa", "Asiantuntijat suosittelevat liikkumaan ainakin 30 minuuttia päivässä.",
+                "Asiantuntijat suosittelevat liikkumaan ainakin 30 minuuttia päivässä.",
+                System.currentTimeMillis() - (r.nextInt(5) * 1000 * 60 * 60 * 24), System.currentTimeMillis(), newsPicData, r.nextInt(200),
+                Collections.singletonList(terveys), getRandomToimittaja());
+
+        uutisRepository.save(u);
+    }
+
+    private void setUpUrheilu(byte[] newsPicData){
+        Uutinen u = new Uutinen("Suomalainen jalkapalloilija sai kultaa", "Suomalainen jalkapalloilija Tero teronen on voittanut Lontoon kisoissa kultaa.",
+                "Suomalainen jalkapalloilija Tero teronen on voittanut vuoden 2017 Lontoon talvikisoissa. Hän on osa UNI-spört joukkuetta. Hän ei vastannut haastattelupyyntöihimme.",
+                System.currentTimeMillis() - (r.nextInt(5) * 1000 * 60 * 60 * 24), System.currentTimeMillis(), newsPicData, r.nextInt(200),
+                Collections.singletonList(urheilu), getRandomToimittaja());
+
+        uutisRepository.save(u);
+
+        u = new Uutinen("Suomalainen urheilu kiinnostaa yhä useamipia ulkomaalaisia", "Etenkin pohjoismaissa Suomalaisen urheilun suosio on kasvanut",
+                "Suomalainen urheilu kiinnostaa yhä useampia ulkomaalaisia, tiedottaa Euroopan urheiluliitto EUSPORT. Etenkin pohjoismainen urheilu näyttää olevan nousussa.",
+                System.currentTimeMillis() - (r.nextInt(5) * 1000 * 60 * 60 * 24), System.currentTimeMillis(), newsPicData, r.nextInt(200),
+                Collections.singletonList(urheilu), getRandomToimittaja());
+
+        uutisRepository.save(u);
+
+        u = new Uutinen("Venäjällä suuri doping-skandaali", "Kolme miesten olympiamitalistia on jäänyt kiinni dopingista.",
+                "Venäjällä 3 miesten pyöräilyn olympiamitalistia vuoden 2015 kesäkisojen pyöräilyssä, on jäänyt kiinni kiellettyjen aineiden käytöstä. Asia huomattiin vuosittaisissa uusinta testeissä.",
+                System.currentTimeMillis() - (r.nextInt(5) * 1000 * 60 * 60 * 24), System.currentTimeMillis(), newsPicData, r.nextInt(200),
+                Collections.singletonList(urheilu), getRandomToimittaja());
+
+        uutisRepository.save(u);
+
+        u = new Uutinen("Onko E-urheilu urheilua?", "Keskustelupalstojen puheenaihe on ollut valtamediassa suuresti esillä.",
+                "Keskustelupalstojen puheenaihe on ollut valtamediassa suuresti esillä. Mitä urheilu oikein on? Urheiluksi lasketaan usein myös Golf tai Pokeri.",
+                System.currentTimeMillis() - (r.nextInt(5) * 1000 * 60 * 60 * 24), System.currentTimeMillis(), newsPicData, r.nextInt(200),
+                Collections.singletonList(urheilu), getRandomToimittaja());
+
+        uutisRepository.save(u);
+
+        u = new Uutinen("3 Asiaa jotka vain urheilijat ymmärtävät", "Klikkaa selvittääksesi oletko urheilija!",
+                "1. Urheilun jälkeen maistuu ruoka. 2. Aina ei vain jaksa. 3. Urheilua ei lasketa ellet ota kuvaa someen",
+                System.currentTimeMillis() - (r.nextInt(5) * 1000 * 60 * 60 * 24), System.currentTimeMillis(), newsPicData, r.nextInt(200),
+                Collections.singletonList(urheilu), getRandomToimittaja());
+
+        uutisRepository.save(u);
+
+        u = new Uutinen("Oletko aina urheillut väärin?", "Asiantuntija kertoo kuinka ammattilaiset urheilevat.",
+                "Ammattilaiset urheilevat säännöllisesti ja rutiininomaisesti.",
+                System.currentTimeMillis() - (r.nextInt(5) * 1000 * 60 * 60 * 24), System.currentTimeMillis(), newsPicData, r.nextInt(200),
+                Collections.singletonList(urheilu), getRandomToimittaja());
+
         uutisRepository.save(u);
     }
     
