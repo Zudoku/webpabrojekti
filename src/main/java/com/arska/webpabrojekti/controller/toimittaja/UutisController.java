4a008f935f67c4dc31764ef19de9d35217d667a5
Arap <arttu.siren@gmail.com>
2017-12-11 14:04:44 +0200

    Lisää Uutisten muokkaamis sivu

--- src/main/java/com/arska/webpabrojekti/controller/toimittaja/UutisController.java
+++ src/main/java/com/arska/webpabrojekti/controller/toimittaja/UutisController.java
@@ -9,6 +9,7 @@ package com.arska.webpabrojekti.controller.toimittaja;
 import static com.arska.webpabrojekti.controller.toimittaja.MuokkausController.TOIMITTAJAID;
 import static com.arska.webpabrojekti.controller.toimittaja.MuokkausController.needLogin;
 import com.arska.webpabrojekti.domain.Uutinen;
+import com.arska.webpabrojekti.service.KategoriaService;
 import com.arska.webpabrojekti.service.ToimittajaService;
 import com.arska.webpabrojekti.service.UutisService;
 import javax.servlet.http.HttpSession;
@@ -32,8 +33,11 @@ public class UutisController {
     @Autowired
     private UutisService uutisService;
     
+    @Autowired
+    private KategoriaService kategoriaService;
+    
     @GetMapping("/toimittaja/uutiset")
-    public String handleGetKategoriat(Model model, HttpSession session) {
+    public String handleGetUutiset(Model model, HttpSession session) {
         if (needLogin(session) || toimittajaService.getToimittaja((long) session.getAttribute(TOIMITTAJAID)) == null) {
             return "redirect:/toimittaja";
         } else {
@@ -41,6 +45,19 @@ public class UutisController {
             model.addAttribute("toimittaja", toimittajaService.getToimittaja(sessionToimittajaID));
             model.addAttribute("uutiset", uutisService.getAllUutinen());
             return "editor/uutiset";
+        }
+    }
+    
+    @GetMapping("/toimittaja/uutinen/{id}")
+    public String handleGetUutinen(Model model, HttpSession session, @PathVariable long id) {
+        if (needLogin(session) || toimittajaService.getToimittaja((long) session.getAttribute(TOIMITTAJAID)) == null) {
+            return "redirect:/toimittaja";
+        } else {
+            long sessionToimittajaID = (long) session.getAttribute(TOIMITTAJAID);
+            model.addAttribute("toimittaja", toimittajaService.getToimittaja(sessionToimittajaID));
+            model.addAttribute("uutinen", uutisService.getUutinen(id));
+            model.addAttribute("kategoriat", kategoriaService.getAllCategories());
+            return "editor/uutinen";
         }
     }
 }
